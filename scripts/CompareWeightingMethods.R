cbd0ca000d505592344f483abf40d0b0bb3c5320
HaediThelen <haedi.thelen@pennmedicine.upenn.edu>
2024-03-25 13:26:06 -0400

    Outline for analysis.

---
+++ scripts/CompareWeightingMethods.R
@@ -0,0 +1,44 @@
+# Balaning weights Methods Review
+
+#Libraries
+
+#1. Load and Prepare Data
+#1a. label the needed variables 
+
+
+#2. Estimate IPTW weights
+#2a. Fit PS model
+
+#2b. Estimate weights
+
+#2c. Evaluate balance
+
+#2d. Calculate effective sample size
+
+
+#3. Estimate Balancing Weights
+#3a. Prepare data for BalanceR
+
+
+#3b. Estimate Balancing weights for lambda = 0, 0.5, 1, 2
+## process for ATT and save each in a new column
+
+#3c. Evaluate Balance
+
+#3d. Calculate effective sample size for each value of labmda 
+
+
+#4. Create Balance Plot 
+#4a. Prepare a datafram with jsut the balance we need
+
+#4b. Plot
+
+
+#5. Create Effective Sample size table
+
+
+#6. Estimate ATT
+#6a.Poisson model with IPTWs 
+
+#6b.Poisson model with Balancing weights for each value of lambda  
+
